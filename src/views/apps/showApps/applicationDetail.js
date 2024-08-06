5a9f701fcb1400d1f77d32f722685e176fa8bc77
boss4edu <boss4edu@gmail.com>
2024-08-06 20:02:41 +0300

    update

--- src/views/apps/showApps/applicationDetail.js
+++ src/views/apps/showApps/applicationDetail.js
@@ -434,10 +434,7 @@ const ApplicationDetails = () => {
                         <Label for="type">Student Type:</Label>
                         <p>{selectedType}</p>
                       </FormGroup>
-                      <FormGroup>
-                        <Label for="type">Academic Degree:</Label>
-                        <p>{academicDegree}</p>
-                      </FormGroup>
+                   
                       <FormGroup>
                         <Label for="type">Semester:</Label>
                         <p>{semester}</p>
@@ -610,7 +607,7 @@ const ApplicationDetails = () => {
               <h4>Initial Acceptance</h4>
              
               <hr />
-              <h5>Initial Acceptances:</h5>
+        
               <Table responsive>
                 <thead>
                   <tr>
@@ -646,7 +643,7 @@ const ApplicationDetails = () => {
               <h4>Final Acceptance</h4>
              
               <hr />
-              <h5>Final Acceptances:</h5>
+            
               <Table responsive>
                 <thead>
                   <tr>
